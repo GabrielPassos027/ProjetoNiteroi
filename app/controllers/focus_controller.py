90ba9e7414e0e39840e07c26587a404abcff04f5
Gabriel De Souza Passos <bielpassos@hotmail.com>
2024-06-28 18:00:21 -0300

    FOCUS fixed

--- app/controllers/focus_controller.py
+++ app/controllers/focus_controller.py
@@ -1,56 +1,219 @@
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask import current_app
+import pandas as pd
 
 def fetch_selic_data():
-    url_selic = "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativasMercadoSelic?%24format=json"
+    url_selic = "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativasMercadoAnuais?%24format=json&%24filter=Indicador%20eq%20'Selic'"
     try:
         response = requests.get(url_selic)
         response.raise_for_status()
         data = response.json().get('value', [])
-        filtered_data = [entry for entry in data if datetime.strptime(entry['Data'], '%Y-%m-%d') >= datetime(2019, 1, 2)]
-        sorted_data = sorted(filtered_data, key=lambda x: x['Data'], reverse=True)
-        return sorted_data
+
+        # Converte a lista de dicionários em DataFrame
+        df = pd.DataFrame(data)
+
+        # Filtra a partir de 04/01/2019
+        start_date = datetime(2019, 1, 4)
+        df['Data'] = pd.to_datetime(df['Data'])
+        df = df[df['Data'] >= start_date]
+
+        # Função para ajustar para a próxima segunda-feira se for fim de semana
+        def adjust_to_weekday(date):
+            if date.weekday() == 5:  # Sábado
+                return date + timedelta(days=2)
+            elif date.weekday() == 6:  # Domingo
+                return date + timedelta(days=1)
+            return date
+
+        # Lista para armazenar as datas filtradas
+        filtered_dates = []
+        current_date = start_date
+
+        while current_date <= datetime.now():
+            adjusted_date = adjust_to_weekday(current_date)
+            filtered_dates.append(adjusted_date)
+            current_date += timedelta(days=7)
+
+        # Filtra os dados pelas datas ajustadas
+        filtered_data = df[df['Data'].isin(filtered_dates)]
+
+        # Seleciona os 4 primeiros dados de cada dia filtrado
+        final_data = []
+        for date in filtered_dates:
+            daily_data = filtered_data[filtered_data['Data'] == date]
+            final_data.extend(daily_data.head(4).to_dict('records'))
+
+        # Formata a coluna 'Data' para remover a hora
+        for entry in final_data:
+            entry['Data'] = entry['Data'].strftime('%Y-%m-%d')
+
+        # Ordena os dados pela data em ordem decrescente
+        final_data = sorted(final_data, key=lambda x: x['Data'], reverse=True)
+
+        return final_data
+
     except requests.RequestException as e:
         current_app.logger.error(f"Erro ao obter dados SELIC: {e}")
         return []
 
 def fetch_pib_data():
-    url_pib = "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativasMercadoTrimestrais?%24format=json&%24filter=Indicador%20eq%20'PIB%20Total'"
+    url_pib = "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativasMercadoAnuais?%24format=json&%24filter=Indicador%20eq%20'PIB%20Total'"
     try:
         response = requests.get(url_pib)
         response.raise_for_status()
         data = response.json().get('value', [])
-        filtered_data = [entry for entry in data if datetime.strptime(entry['Data'], '%Y-%m-%d') >= datetime(2019, 1, 2)]
-        sorted_data = sorted(filtered_data, key=lambda x: x['Data'], reverse=True)
-        return sorted_data
+
+        # Converte a lista de dicionários em DataFrame
+        df = pd.DataFrame(data)
+
+        # Filtra a partir de 04/01/2019
+        start_date = datetime(2019, 1, 4)
+        df['Data'] = pd.to_datetime(df['Data'])
+        df = df[df['Data'] >= start_date]
+
+        # Função para ajustar para a próxima segunda-feira se for fim de semana
+        def adjust_to_weekday(date):
+            if date.weekday() == 5:  # Sábado
+                return date + timedelta(days=2)
+            elif date.weekday() == 6:  # Domingo
+                return date + timedelta(days=1)
+            return date
+
+        # Lista para armazenar as datas filtradas
+        filtered_dates = []
+        current_date = start_date
+
+        while current_date <= datetime.now():
+            adjusted_date = adjust_to_weekday(current_date)
+            filtered_dates.append(adjusted_date)
+            current_date += timedelta(days=7)
+
+        # Filtra os dados pelas datas ajustadas
+        filtered_data = df[df['Data'].isin(filtered_dates)]
+
+        # Seleciona os dados do quinto ao nono de cada dia filtrado
+        final_data = []
+        for date in filtered_dates:
+            daily_data = filtered_data[filtered_data['Data'] == date]
+            final_data.extend(daily_data.iloc[5:9].to_dict('records'))
+
+        # Formata a coluna 'Data' para remover a hora
+        for entry in final_data:
+            entry['Data'] = entry['Data'].strftime('%Y-%m-%d')
+
+        # Ordena os dados pela data em ordem decrescente
+        final_data = sorted(final_data, key=lambda x: x['Data'], reverse=True)
+
+        return final_data
     except requests.RequestException as e:
         current_app.logger.error(f"Erro ao obter dados PIB: {e}")
         return []
     
 def fetch_focus_ipca_data():
-    url_ipca = "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativaMercadoMensais?%24format=json&%24filter=Indicador%20eq%20'IPCA'"
+    url_ipca = "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativasMercadoAnuais?%24format=json&%24filter=Indicador%20eq%20'IPCA'"
     try:
         response = requests.get(url_ipca)
         response.raise_for_status()
         data = response.json().get('value', [])
-        filtered_data = [entry for entry in data if datetime.strptime(entry['Data'], '%Y-%m-%d') >= datetime(2019, 1, 2)]
-        sorted_data = sorted(filtered_data, key=lambda x: x['Data'], reverse=True)
-        return sorted_data
+
+        # Converte a lista de dicionários em DataFrame
+        df = pd.DataFrame(data)
+
+        # Filtra a partir de 04/01/2019
+        start_date = datetime(2019, 1, 4)
+        df['Data'] = pd.to_datetime(df['Data'])
+        df = df[df['Data'] >= start_date]
+
+        # Função para ajustar para a próxima segunda-feira se for fim de semana
+        def adjust_to_weekday(date):
+            if date.weekday() == 5:  # Sábado
+                return date + timedelta(days=2)
+            elif date.weekday() == 6:  # Domingo
+                return date + timedelta(days=1)
+            return date
+
+        # Lista para armazenar as datas filtradas
+        filtered_dates = []
+        current_date = start_date
+
+        while current_date <= datetime.now():
+            adjusted_date = adjust_to_weekday(current_date)
+            filtered_dates.append(adjusted_date)
+            current_date += timedelta(days=7)
+
+        # Filtra os dados pelas datas ajustadas
+        filtered_data = df[df['Data'].isin(filtered_dates)]
+
+        # Seleciona os dados do quinto ao nono de cada dia filtrado
+        final_data = []
+        for date in filtered_dates:
+            daily_data = filtered_data[filtered_data['Data'] == date]
+            final_data.extend(daily_data.iloc[5:9].to_dict('records'))
+
+        # Formata a coluna 'Data' para remover a hora
+        for entry in final_data:
+            entry['Data'] = entry['Data'].strftime('%Y-%m-%d')
+
+        # Ordena os dados pela data em ordem decrescente
+        final_data = sorted(final_data, key=lambda x: x['Data'], reverse=True)
+
+        return final_data
     except requests.RequestException as e:
-        current_app.logger.error(f"Erro ao obter dados IPCA/Câmbio: {e}")
+        current_app.logger.error(f"Erro ao obter dados IPCA: {e}")
         return []
     
 def fetch_focus_cambio_data():
-    url_cambio = "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativaMercadoMensais?%24format=json&%24filter=Indicador%20eq%20'C%C3%A2mbio'"
+    url_cambio = "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativasMercadoAnuais?%24format=json&%24filter=Indicador%20eq%20'C%C3%A2mbio'"
     try:
         response = requests.get(url_cambio)
         response.raise_for_status()
         data = response.json().get('value', [])
-        filtered_data = [entry for entry in data if datetime.strptime(entry['Data'], '%Y-%m-%d') >= datetime(2019, 1, 2)]
-        sorted_data = sorted(filtered_data, key=lambda x: x['Data'], reverse=True)
-        return sorted_data
+
+        # Converte a lista de dicionários em DataFrame
+        df = pd.DataFrame(data)
+
+        # Filtra a partir de 04/01/2019
+        start_date = datetime(2019, 1, 4)
+        df['Data'] = pd.to_datetime(df['Data'])
+        df = df[df['Data'] >= start_date]
+
+        # Função para ajustar para a próxima segunda-feira se for fim de semana
+        def adjust_to_weekday(date):
+            if date.weekday() == 5:  # Sábado
+                return date + timedelta(days=2)
+            elif date.weekday() == 6:  # Domingo
+                return date + timedelta(days=1)
+            return date
+
+        # Lista para armazenar as datas filtradas
+        filtered_dates = []
+        current_date = start_date
+
+        while current_date <= datetime.now():
+            adjusted_date = adjust_to_weekday(current_date)
+            filtered_dates.append(adjusted_date)
+            current_date += timedelta(days=7)
+
+        # Filtra os dados pelas datas ajustadas
+        filtered_data = df[df['Data'].isin(filtered_dates)]
+
+        # Seleciona os dados do quinto ao nono de cada dia filtrado
+        final_data = []
+        for date in filtered_dates:
+            daily_data = filtered_data[filtered_data['Data'] == date]
+            final_data.extend(daily_data.iloc[5:9].to_dict('records'))
+
+        # Formata a coluna 'Data' para remover a hora
+        for entry in final_data:
+            entry['Data'] = entry['Data'].strftime('%Y-%m-%d')
+
+        # Ordena os dados pela data em ordem decrescente
+        final_data = sorted(final_data, key=lambda x: x['Data'], reverse=True)
+
+        return final_data
+
     except requests.RequestException as e:
-        current_app.logger.error(f"Erro ao obter dados IPCA/Câmbio: {e}")
+        current_app.logger.error(f"Erro ao obter dados Câmbio: {e}")
         return []
